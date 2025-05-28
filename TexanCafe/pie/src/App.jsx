d3205e7f5646a8f0dac39694028d78f5582683cc
Tyler Lane <tyler.d.lane94@gmail.com>
2025-05-28 16:50:19 -0500

    Made list for Pies

---
+++ TexanCafe/pie/src/App.jsx
@@ -0,0 +1,46 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='container'>
+        <div id="pielist">
+          <ul className="pie">Pie</ul>
+          <ul className="pie">Pie</ul>
+          <ul className="pie">Pie</ul>
+          <ul className="pie">Pie</ul>
+          <ul className="pie">Pie</ul>
+          <ul className="pie">Pie</ul>
+          <ul className="pie">Pie</ul>
+          <ul className="pie">Pie</ul>
+          <ul className="pie">Pie</ul>
+          <ul className="pie">Pie</ul>
+          <ul className="pie">Pie</ul>
+          <ul className="pie">Pie</ul>
+          <ul className="pie">Pie</ul>
+          <ul className="pie">Pie</ul>
+          <ul className="pie">Pie</ul>
+          <ul className="pie">Pie</ul>
+          <ul className="pie">Pie</ul>
+          <ul className="pie">Pie</ul>
+          <ul className="pie">Pie</ul>
+          <ul className="pie">Pie</ul>
+          <ul className="pie">Pie</ul>
+          <ul className="pie">Pie</ul>
+          <ul className="pie">Pie</ul>
+          <ul className="pie">Pie</ul>
+          <ul className="pie">Pie</ul>
+          <ul className="pie">Pie</ul>
+          <ul className="pie">Pie</ul>
+          <ul className="pie">Pie</ul>
+          <ul className="pie">Pie</ul>
+          <ul className="pie">Pie</ul>
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
