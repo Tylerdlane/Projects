03ce5b36a2b97889adfea891ec1cf9f3989482c7
Tyler Lane <tyler.d.lane94@gmail.com>
2025-05-28 19:56:05 -0500

    Fixing list and added img

--- TexanCafe/pie/src/App.jsx
+++ TexanCafe/pie/src/App.jsx
@@ -1,13 +1,21 @@
 import './App.css'
+import pie1 from './assets/pie1.jpg';
+
+
 
 function App() {
 
   return (
     <>
+    <div className="container">
+      <h1>
+        Pie Shop
+      </h1>
+    </div>
       <div className='container'>
         <div id="pielist">
           <ul className="differentPies">
-            <li className="pie"></li>
+            <li className="pie"><img src={pie1} alt="Pie 1" /></li>
             <li className="pie"></li>
             <li className="pie"></li>
             <li className="pie"></li>
