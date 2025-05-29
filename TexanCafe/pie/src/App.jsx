75a836db136a30fd9c5499e96a8fbad8cb641b11
Tyler Lane <tyler.d.lane94@gmail.com>
2025-05-28 20:47:36 -0500

    added img slider and img to list

--- TexanCafe/pie/src/App.jsx
+++ TexanCafe/pie/src/App.jsx
@@ -1,5 +1,10 @@
 import './App.css'
 import pie1 from './assets/pie1.jpg';
+import pie2 from './assets/pie2.jpg';
+import pie3 from './assets/pie3.jpg';
+import pie4 from './assets/pie4.jpg';
+import pie5 from './assets/pie5.jpg';
+
 
 
 
@@ -16,10 +21,10 @@ function App() {
         <div id="pielist">
           <ul className="differentPies">
             <li className="pie"><img src={pie1} alt="Pie 1" /></li>
-            <li className="pie"></li>
-            <li className="pie"></li>
-            <li className="pie"></li>
-            <li className="pie"></li>
+            <li className="pie"><img src={pie2} alt="Pie 2" /></li>
+            <li className="pie"><img src={pie3} alt="Pie 3" /></li>
+            <li className="pie"><img src={pie4} alt="Pie 4" /></li>
+            <li className="pie"><img src={pie5} alt="Pie 5" /></li>
             <li className="pie"></li>
             <li className="pie"></li>
             <li className="pie"></li>
@@ -36,9 +41,14 @@ function App() {
             <li className="pie"></li>
             <li className="pie"></li>
           </ul>
-          
         </div>
-
+      </div>
+      <div className="imgcontainer">
+        <img src={pie1} alt="Pie 1" />
+        <img src={pie2} alt="Pie 2" />
+        <img src={pie3} alt="Pie 3" />
+        <img src={pie4} alt="Pie 4" />
+        <img src={pie5} alt="Pie 5" />
       </div>
     </>
   )
