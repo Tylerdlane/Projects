18efc892f6ea2befd735ec0cfa34d69c8e375c96
Tyler Lane <tyler.d.lane94@gmail.com>
2025-12-08 18:08:49 -0600

    adding carousel

--- TexanCafe/pie/src/components/navBar.jsx
+++ TexanCafe/pie/src/components/navBar.jsx
@@ -1,29 +1,83 @@
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Logo from "../assets/logo.jpg";
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">Texan Cafe and Pie Shop</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">Home</Link>
+  <nav className="navbar navbar-expand-lg bg-body-tertiary mb-4 w-100 shadow-lg">
+  <div className="container-fluid">
+    <Link className="navbar-brand" to="/">Texan Cafe and Pie Shop</Link>
+ {/* Center logo */}
+    <Link className="navbar-brand mx-auto d-none d-lg-block" to="/">
+      <img 
+        src={Logo} 
+        alt="Logo" 
+        style={{ height: "60px" }}
+      />
+    </Link>
+
+    {/* Mobile logo (shows in collapsed menu area) */}
+    <Link className="navbar-brand d-lg-none" to="/">
+      <img 
+        src={Logo} 
+        alt="Logo" 
+        style={{ height: "50px" }}
+      />
+    </Link>
+    <button 
+      className="navbar-toggler" 
+      type="button" 
+      data-bs-toggle="collapse" 
+      data-bs-target="#navbarNav"
+    >
+      <span className="navbar-toggler-icon"></span>
+    </button>
+
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav ms-auto">
+
+        <li className="nav-item">
+          <Link className="nav-link" to="/">Home</Link>
+        </li>
+
+        <li className="nav-item">
+          <Link className="nav-link" to="/pieshop">Pie Shop</Link>
+        </li>
+
+        <li className="nav-item">
+          <Link className="nav-link" to="/locations">Locations</Link>
+        </li>
+
+        {/* Dropdown Menu */}
+        <li className="nav-item dropdown">
+          <a 
+            className="nav-link dropdown-toggle" 
+            href="#" 
+            role="button" 
+            data-bs-toggle="dropdown"
+          >
+            More
+          </a>
+
+          <ul className="dropdown-menu dropdown-menu-end">
+            <li>
+              <Link className="dropdown-item" to="/about">About Us</Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/pieshop">Pie Shop</Link>
+            <li>
+              <Link className="dropdown-item" to="/contact">Contact</Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/locations">Locations</Link>
+            <li>
+              <Link className="dropdown-item" to="/careers">Careers</Link>
             </li>
           </ul>
-        </div>
-      </div>
-    </nav>
+        </li>
+
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
   );
 }
 
--- TexanCafe/pie/src/pages/home.jsx
+++ TexanCafe/pie/src/pages/home.jsx
@@ -1,35 +1,96 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import '../App.css';
-import pie1 from '../assets/pie1.jpg'
-import pie2 from '../assets/pie2.jpg';
-import pie3 from '../assets/pie3.jpg';
-import pie4 from '../assets/pie4.jpg';
-import pie5 from '../assets/pie5.jpg';
-import TayTex from '../assets/TaylorTexan.jpg'
-import HutTex from '../assets/HuttoTexan.jpg'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "../App.css";
+import pie1 from "../assets/pie1.jpg";
+import pie2 from "../assets/pie2.jpg";
+import pie3 from "../assets/pie3.jpg";
+import pie4 from "../assets/pie4.jpg";
+import pie5 from "../assets/pie5.jpg";
+import TayTex from "../assets/TaylorTexan.jpg";
+import HutTex from "../assets/HuttoTexan.jpg";
+import HappyHour from "../assets/HappyHour.png"
 
 function App() {
   return (
     <>
-    <h1 className='title'>Welcome to the Texan Cafe!</h1>
-      <div className="col-12 container dinner">
-    
-        {/* Pie Images Section */}
-        <div className="imgcontainer">
-          <img src={pie1} alt="Pie 1" className="img-fluid rounded" style={{ maxWidth: '300px' }} />
-          <img src={pie2} alt="Pie 2" className="img-fluid rounded" style={{ maxWidth: '300px' }} />
-          <img src={pie3} alt="Pie 3" className="img-fluid rounded" style={{ maxWidth: '300px' }} />
-          <img src={pie4} alt="Pie 4" className="img-fluid rounded" style={{ maxWidth: '300px' }} />
-          <img src={pie5} alt="Pie 5" className="img-fluid rounded" style={{ maxWidth: '300px' }} />
+      <div className="dinner">
+        <h1 className="title">Welcome to the Texan Cafe!</h1>
+        <div className="col-12 container">
+
+         
+          <div id="pieCarousel" className="carousel" data-bs-ride="carousel">
+  <div className="carousel-inner">
+
+    <div className="carousel-item active">
+      <img src={HappyHour} className="d-block rounded" alt="Pie 1" />
+    </div>
+
+  </div>
+
+  {/* Carousel Controls */}
+  <button className="carousel-control-prev" type="button" data-bs-target="#pieCarousel" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+
+  <button className="carousel-control-next" type="button" data-bs-target="#pieCarousel" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+
+         
+          <div className="d-flex justify-content-center gap-3 my-4 mb-5">
+  <button className="btn btn-success btn-lg px-4 mb-5">
+    Order Here
+  </button>
+
+  <button className="btn btn-success btn-lg px-4 mb-5">
+    Pieshop
+  </button>
+</div>
+
+
+          {/* Pie Images Section */}
+          <div className="imgcontainer">
+            <img
+              src={pie1}
+              alt="Pie 1"
+              className="img-fluid rounded"
+              style={{ maxWidth: "300px" }}
+            />
+            <img
+              src={pie2}
+              alt="Pie 2"
+              className="img-fluid rounded"
+              style={{ maxWidth: "300px" }}
+            />
+            <img
+              src={pie3}
+              alt="Pie 3"
+              className="img-fluid rounded"
+              style={{ maxWidth: "300px" }}
+            />
+            <img
+              src={pie4}
+              alt="Pie 4"
+              className="img-fluid rounded"
+              style={{ maxWidth: "300px" }}
+            />
+            <img
+              src={pie5}
+              alt="Pie 5"
+              className="img-fluid rounded"
+              style={{ maxWidth: "300px" }}
+            />
+          </div>
         </div>
-      </div>
 
         {/* Locations Section */}
         <div className="container col-6 mb-5">
           <h3>Locations</h3>
           <div className="locationImg gap-4">
-            <img src={HutTex} alt="Hutto"></img>
+            <img className="locationImg" src={HutTex} alt="Hutto"></img>
             <img src={TayTex} alt="Taylor"></img>
           </div>
         </div>
@@ -38,10 +99,13 @@ function App() {
         <div className="container col-6 mb-5">
           <h3>Get to Know Us!</h3>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa impedit, fugiat id repellendus inventore
-            sapiente numquam, tempore ex officia facilis ut adipisci. Cumque laudantium aperiam ea rem, omnis ut! Error.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa
+            impedit, fugiat id repellendus inventore sapiente numquam, tempore
+            ex officia facilis ut adipisci. Cumque laudantium aperiam ea rem,
+            omnis ut! Error.
           </p>
         </div>
+      </div>
     </>
   );
 }
